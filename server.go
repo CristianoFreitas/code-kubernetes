a3e4409b0d93abb3a93bdc2712b78529b1169f99
CristianoFreitas <cristianofreitas.ti@gmail.com>
2022-03-24 23:13:42 -0300

    using variables

--- server.go
+++ server.go
@@ -1,12 +1,18 @@
 package main
 
-import "net/http"
-	
-func greet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello World v2!</h1>"))
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+	fmt.Fprintf(w, "Hello, I'm %s. I'm %s.", name, age)
 }
 
 func main() {
-	http.HandleFunc("/", greet)
+	http.HandleFunc("/", hello)
 	http.ListenAndServe(":8000", nil)
 }
